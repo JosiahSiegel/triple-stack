9e5815de9f573b199ac13850d508db9893b87690
josiah85 <jsiegel@atni.com>
2015-01-26 11:50:28 -0500

    ldap_user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :ldap_users
   get 'welcome/index'
 
   devise_for :users
---
+++ test/models/ldap_user_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LdapUserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
